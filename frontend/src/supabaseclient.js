b57435207fa5ce96a6c2d153ebddce34c64ae9de
AKSHAY UNNI M V <144668312+akshayunnimv@users.noreply.github.com>
2025-12-19 23:12:51 +0530

    Update supabaseclient.js

--- frontend/src/supabaseclient.js
+++ frontend/src/supabaseclient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = "https://zdaweyukywitilkezfrx.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkYXdleXVreXdpdGlsa2V6ZnJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1NTY3ODUsImV4cCI6MjA1NDEzMjc4NX0.tbX3mLLygGDbjuzjP6cacxHezXDkzt3TL76L7dkA8F0";
+const SUPABASE_URL = "Your url";
+const SUPABASE_ANON_KEY = "Your Anon key";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
